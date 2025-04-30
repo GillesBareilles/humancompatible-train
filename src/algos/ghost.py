fix gradient in ALM, update

src/algos/ghost.py
@@ -9,6 +9,11 @@ from qpsolvers import solve_qp
 import autoray as ar
 import timeit
 from itertools import cycle
+
+from src.algos.constraints import *
+
+from fairret.statistic import *
+from fairret.loss import NormLoss
 from .utils import net_grads_to_tensor, net_params_to_tensor
 import cvxpy as cp
 import torch
@@ -97,24 +102,8 @@ def solvesubp(fgrad, cval, cgrad, kap_val, beta, tau, hesstype, mc, n, qp_solver
       beta*np.ones((n,)),
       qp_solver)
 
-def one_sided_loss_constr(loss, net, c_data):
-    w_inputs, w_labels = c_data[0]
-    b_inputs, b_labels = c_data[1]
-    w_outs = net(w_inputs)
-    if w_labels.ndim == 0:
-        w_labels = w_labels.reshape(1)
-        b_labels = b_labels.reshape(1)
-    else:
-        w_labels = w_labels.unsqueeze(1)
-        b_labels = b_labels.unsqueeze(1)
-    w_loss = loss(w_outs, w_labels)
-    b_outs = net(b_inputs)
-    b_loss = loss(b_outs, b_labels)
 
-    return w_loss - b_loss
-
-
-def StochasticGhost(net, data, w_ind, b_ind, geomp, loss_bound, maxiter, max_runtime=np.inf,
+def StochasticGhost(net, data, w_ind, b_ind, geomp, loss_bound, maxiter, max_runtime=np.inf, c_sample_half=False,
                     stepsize_rule = 'inv_iter', zeta=0.5, gamma0 = 0.1, rho=0.8, lamb=0.5, beta=10., tau=1.,seed=42):
     
     
@@ -122,6 +111,11 @@ def StochasticGhost(net, data, w_ind, b_ind, geomp, loss_bound, maxiter, max_run
     
     c1 = lambda net, d: one_sided_loss_constr(loss_fn, net, d) - loss_bound
     c2 = lambda net, d: -one_sided_loss_constr(loss_fn, net, d) - loss_bound
+    
+    # statistic = PositiveRate()
+    # norm_fairret = NormLoss(statistic)
+    # c1 = lambda net, d: fairret_pr_constr(norm_fairret, net, d) - loss_bound
+    # c2 = lambda net, d: fairret_pr_constr(norm_fairret, net, d) - loss_bound
     
     max_sample_size = max([len(w_ind), len(b_ind)])
     
@@ -178,8 +172,9 @@ def StochasticGhost(net, data, w_ind, b_ind, geomp, loss_bound, maxiter, max_run
         indices_f, indices_c_w, indices_c_b = [],[],[]
         for j, subp_batch_size in enumerate(mbatches):
             idx_f = rng.choice(len(data), size=subp_batch_size)
-            idx_c_w = rng.choice(len(data_w), size=max(subp_batch_size//2, 1))
-            idx_c_b = rng.choice(len(data_b), size=max(subp_batch_size//2, 1))
+            c_bs = max(subp_batch_size//2, 1) if c_sample_half else subp_batch_size
+            idx_c_w = rng.choice(len(data_w), size=c_bs)
+            idx_c_b = rng.choice(len(data_b), size=c_bs)
             if j == 1:
                 indices_f.append(idx_f[::2]) # even
                 indices_f.append(idx_f[1::2]) # odd
@@ -209,13 +204,15 @@ def StochasticGhost(net, data, w_ind, b_ind, geomp, loss_bound, maxiter, max_run
             
             # calculate autograd jacobian of obj fun w.r.t. params
             outs = net(obj_batch[0])
-            feval = loss_fn(outs, obj_batch[1].unsqueeze(1))
+            if obj_batch[1].ndim < outs.ndim:
+                outs = outs.squeeze(1)
+            feval = loss_fn(outs, obj_batch[1])
     
             feval.backward()
             dfdw = net_grads_to_tensor(net, clip=False)
-            net.zero_grad()
             
             # calculate autograd jacobian of constraints fun w.r.t. params
+            net.zero_grad()
             c1_val = c1(net, c1_batch)
             c1_val.backward()
             c1_grad = ar.to_numpy(net_grads_to_tensor(net, clip=False))
@@ -264,7 +261,7 @@ def StochasticGhost(net, data, w_ind, b_ind, geomp, loss_bound, maxiter, max_run
                 
         history['w'].append(deepcopy(net.state_dict()))
         
-        feval = loss_fn(outs, obj_batch[1].unsqueeze(1))
+        feval = loss_fn(outs, obj_batch[1])#.unsqueeze(1))
     
     history['constr'] = pd.DataFrame(history['constr'])
     return history

src/algos/sslpd.py
@@ -8,6 +8,8 @@ from qpsolvers import solve_qp
 import autoray as ar
 import timeit
 from itertools import cycle
+
+from src.algos.constraints import *
 from .utils import net_grads_to_tensor, net_params_to_tensor
 import cvxpy as cp
 import torch
@@ -17,22 +19,6 @@ m_st = 2
 import timeit
 constr_sampling_interval = 1
 max_runtime = 15
-
-def one_sided_loss_constr(loss, net, c_data):
-    w_inputs, w_labels = c_data[0]
-    b_inputs, b_labels = c_data[1]
-    w_outs = net(w_inputs)
-    if w_labels.ndim == 0:
-        w_labels = w_labels.reshape(1)
-        b_labels = b_labels.reshape(1)
-    else:
-        w_labels = w_labels.unsqueeze(1)
-        b_labels = b_labels.unsqueeze(1)
-    w_loss = loss(w_outs, w_labels)
-    b_outs = net(b_inputs)
-    b_loss = loss(b_outs, b_labels)
-
-    return w_loss - b_loss
 
 
 def project(x, m):
@@ -50,7 +36,8 @@ def SSLPD(net: torch.nn.Module, data, w_ind, b_ind, loss_bound,
             mu = 2.,
             tau = 1e-4,
             beta = 0.1,
-            eta = 1e-3,
+            # eta = 1e-3,
+            eta = 5e-2,
             B = 1000,
             start_lambda=None,
             device='cpu',
@@ -61,7 +48,8 @@ def SSLPD(net: torch.nn.Module, data, w_ind, b_ind, loss_bound,
     history = {'loss': [],
                'constr': [],
                'w': [],
-               'time': []}
+               'time': [],
+               'n_samples': []}
     
     # slack variables
     slack_vars = torch.zeros(2, requires_grad=True)
@@ -99,6 +87,7 @@ def SSLPD(net: torch.nn.Module, data, w_ind, b_ind, loss_bound,
             current_time = timeit.default_timer()
             history['w'].append(deepcopy(net.state_dict()))
             history['time'].append(current_time - run_start)
+            history['n_samples'].append(batch_size*3)
             if max_runtime > 0 and current_time - run_start >= max_runtime:
                 break
             
@@ -169,8 +158,8 @@ def SSLPD(net: torch.nn.Module, data, w_ind, b_ind, loss_bound,
                 for i in range(len(slack_vars)):
                     slack_vars[i] = x_t1[i-len(slack_vars)]
                     
-            with np.printoptions(precision=6, suppress=True):
-                print(f"""{iteration}|{loss_eval.detach().cpu().numpy()}|{_lambda.detach().cpu().numpy()}|{c_2.detach().cpu().numpy()}|{slack_vars.detach().cpu().numpy()}""", end='\r')
+            with np.printoptions(precision=6, suppress=True, floatmode='fixed'):
+                print(f"""{iteration:5}|{loss_eval.detach().cpu().numpy()}|{_lambda.detach().cpu().numpy()}|{c_2.detach().cpu().numpy()}|{slack_vars.detach().cpu().numpy()}""", end='\r')
         
     ######################
     ### POSTPROCESSING ###    
@@ -227,8 +216,9 @@ def SSLPD(net: torch.nn.Module, data, w_ind, b_ind, loss_bound,
         _set_weights(net, x_t1)
     
     current_time = timeit.default_timer()
-    history['time'].append(current_time - run_start)
-    history['w'].append(deepcopy(net.state_dict()))
+    # history['time'].append(current_time - run_start)
+    # history['w'].append(deepcopy(net.state_dict()))
+    # history['batch_size']
     
     return history
 

src/algos/sw_sub.py
@@ -8,6 +8,8 @@ from qpsolvers import solve_qp
 import autoray as ar
 import timeit
 from itertools import cycle
+
+from src.algos.constraints import *
 from .utils import net_grads_to_tensor, net_params_to_tensor
 import cvxpy as cp
 import torch
@@ -18,129 +20,11 @@ import timeit
 constr_sampling_interval = 1
 max_runtime = 15
 
-def one_sided_loss_constr(loss, net, c_data):
-    w_inputs, w_labels = c_data[0]
-    b_inputs, b_labels = c_data[1]
-    w_outs = net(w_inputs)
-    if w_labels.ndim == 0:
-        w_labels = w_labels.reshape(1)
-        b_labels = b_labels.reshape(1)
-    else:
-        w_labels = w_labels.unsqueeze(1)
-        b_labels = b_labels.unsqueeze(1)
-    w_loss = loss(w_outs, w_labels)
-    b_outs = net(b_inputs)
-    b_loss = loss(b_outs, b_labels)
-
-    return w_loss - b_loss
-
-
 def project(x, m):
     # for i in range(1,m+1):
     #     if x[-i] < 0:
     #         x[-i] = 0
     return x
-
-
-# def SwitchingSubgradient_slack(net: torch.nn.Module, data, w_ind, b_ind, loss_bound,
-#             batch_size=8,
-#             max_runtime=np.inf,
-#             ctol = 5e-4,
-#             start_lambda=None,
-#             device='cpu',
-#             epochs=2,
-#             seed = 42):
-        
-#     history = {'loss': [],
-#                'constr': [],
-#                'w': []}
-    
-#     # slack variables
-#     slack_vars = torch.zeros(2, requires_grad=True)
-    
-#     c1 = lambda net, d, s: one_sided_loss_constr(loss_fn, net, d) - loss_bound + s
-#     c2 = lambda net, d, s: -one_sided_loss_constr(loss_fn, net, d) - loss_bound + s
-    
-#     data_w = torch.utils.data.Subset(data, w_ind)
-#     data_b = torch.utils.data.Subset(data, b_ind)
-    
-#     loss_fn = torch.nn.BCEWithLogitsLoss()
-#     loss_eval = None
-#     c_t = None
-#     run_start = timeit.default_timer()
-#     for epoch in range(epochs):
-        
-#         gen = torch.Generator(device=device)
-#         gen.manual_seed(seed+epoch)
-#         loader = torch.utils.data.DataLoader(data, batch_size, shuffle=True, generator=gen)
-#         loader_w = cycle(torch.utils.data.DataLoader(data_w, batch_size, shuffle=True, generator=gen))
-#         loader_b = cycle(torch.utils.data.DataLoader(data_b, batch_size, shuffle=True, generator=gen))
-        
-#         for iteration, f_sample in enumerate(loader):
-            
-#             current_time = timeit.default_timer()
-#             if max_runtime > 0 and current_time - run_start >= max_runtime:
-#                 print(current_time - run_start)
-#                 return
-            
-#             net.zero_grad()
-            
-#             eta_t = 1e-3
-            
-#             # generate sample of constraints
-#             cw_sample = next(loader_w)
-#             cb_sample = next(loader_b)
-#             c_sample = [cw_sample, cb_sample]
-#             c1_eval = c1(net, c_sample, slack_vars[0])
-#             c2_eval = c2(net, c_sample, slack_vars[1])
-#             c_t = torch.concat([
-#                 c1_eval.reshape(1),
-#                 c2_eval.reshape(1)
-#             ])
-#             c_max = torch.max(c_t)
-
-#             x_t = torch.concat([
-#                 net_params_to_tensor(net, flatten=True, copy=True),
-#                 slack_vars
-#             ])
-            
-#             if c_max >= ctol:
-#                 # generate stochastic gradient
-#                 c_max.backward()
-#                 c_grad = net_grads_to_tensor(net)
-#                 c_grad = torch.concat([c_grad, slack_vars.grad])
-#                 x_t1 = project(x_t - eta_t*c_grad, m=2)
-#             else:
-#                 f_inputs, f_labels = f_sample
-#                 outputs = net(f_inputs)       
-#                 if f_labels.dim() < outputs.dim():
-#                     f_labels = f_labels.unsqueeze(1)
-#                 loss_eval = loss_fn(outputs, f_labels)
-#                 loss_eval.backward()
-#                 f_grad = net_grads_to_tensor(net)
-#                 f_grad = torch.concat([f_grad, torch.zeros(2)]) # add zeros for slack vars
-                
-#                 x_t1 = project(x_t - eta_t*f_grad, m=2)
-            
-#             start = 0
-#             with torch.no_grad():
-#                 w = net_params_to_tensor(net, flatten=False, copy=False)
-#                 for i in range(len(w)):
-#                     end = start + w[i].numel()
-#                     w[i].set_(x_t1[start:end].reshape(w[i].shape))
-#                     start = end
-
-#                 for i in range(len(slack_vars)):
-#                     slack_vars[i] = x_t1[i-len(slack_vars)]
-            
-#             if loss_eval is not None and c_t is not None:
-#                 print(f"""{iteration}|{loss_eval.detach().cpu().numpy()}|{c_t.detach().cpu().numpy()}|{slack_vars.detach().cpu().numpy()}""", end='\r')
-#             history['w'].append(deepcopy(net.state_dict()))
-        
-#     ######################
-#     ### POSTPROCESSING ###    
-#     ######################
-#     return history
 
 
 
@@ -238,6 +122,8 @@ def SwitchingSubgradient(net: torch.nn.Module, data, w_ind, b_ind, loss_bound,
                     c_eta_t = c_max / torch.norm(c_grad)**2
                 elif c_stepsize_rule == 'const':
                     c_eta_t = c_stepsize
+                elif c_stepsize_rule == 'dimin':
+                    c_eta_t = c_stepsize / np.sqrt(c_iters)
                 
                 x_t1 = project(x_t - c_eta_t*c_grad, m=2)
             else:

src/experiments/run_folktables.py
@@ -11,9 +11,10 @@ from torch.utils.data import TensorDataset, DataLoader
 parent_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(os.path.dirname(parent_dir)))
         
+from src.algos.constraints import one_sided_loss_constr
 from src.algos.sslpd import SSLPD
 from src.algos.sw_sub import SwitchingSubgradient_unbiased
-from src.algos.auglag import AugLagr
+from src.algos.auglag import *
 from src.algos.ghost import StochasticGhost
 
 class SimpleNet(nn.Module):
@@ -45,24 +46,6 @@ class SimpleDiffNet(nn.Module):
     def forward(self, x):
         logits = self.linear_gelu_stack(x)
         return logits
-
-def one_sided_loss_constr(loss, net, c_data):
-    w_inputs, w_labels = c_data[0]
-    b_inputs, b_labels = c_data[1]
-    w_outs = net(w_inputs)
-    w_loss = loss(w_outs, w_labels.unsqueeze(1))
-    b_outs = net(b_inputs)
-    b_loss = loss(b_outs, b_labels.unsqueeze(1))
-
-    return w_loss - b_loss
-
-def roc_constraint(loss, net, c_data):
-    w_inputs, w_labels = c_data[0]
-    b_inputs, b_labels = c_data[1]
-    w_outs = net(w_inputs)
-    b_outs = net(b_inputs)
-    
-    # thresholds = 
     
 
 
@@ -76,6 +59,10 @@ if __name__ == "__main__":
     ### experiment parameters
     parser.add_argument('-alg', '--algorithm')
     parser.add_argument('-ne', '--num_exp', type=int)
+    parser.add_argument('-task', '--task', type=str)
+    parser.add_argument('-state', '--state', type=str)
+    parser.add_argument('-loss_bound', '--loss_bound', type=float)
+    parser.add_argument('-constraint', '--constraint', type=str)
     
     ### algorithm parameters
     parser.add_argument('-maxiter', '--maxiter', nargs='?', const=None, type=int)
@@ -89,21 +76,29 @@ if __name__ == "__main__":
     parser.add_argument('-tau', '--tau', nargs='?', const=1, default=1, type=float)
     parser.add_argument('-stepsize', '-sr', nargs='?', const='inv_iter', default='inv_iter', type=str)
     
+    
+    # alm
+    parser.add_argument('-bs', '--batch_size', nargs='?', const=16, default=16, type=int)
+    
     # ssg
     parser.add_argument('-frule', '--frule', nargs='?', const='dimin', default='dimin', type=str)
     parser.add_argument('-fs', '--f_stepsize', nargs='?', const=7e-1, default=7e-1, type=float)
     parser.add_argument('-crule', '--crule', nargs='?', const='dimin', default='dimin', type=str)
     parser.add_argument('-cs', '--c_stepsize', nargs='?', const=7e-1, default=7e-1, type=float)
-    parser.add_argument('-e', '--epochs', nargs='?', const=1, default=1, type=int)
-    parser.add_argument('-ctol', '--ctol', nargs='?', const=1e-3, default=1e-3, type=float)
+    parser.add_argument('-epochs', '--epochs', nargs='?', const=1, default=1, type=int)
+    parser.add_argument('-ctol', '--ctol', nargs=1, type=float)
     
-    # alm
-    parser.add_argument('-bs', '--batch_size', nargs='?', const=8, default=8, type=int)
+    # sslalm
+    parser.add_argument('-mu', '--mu', nargs='?', const=2, default=2, type=float)
     
     # parse args
     args = parser.parse_args()
     ALG_TYPE = args.algorithm
     EXP_NUM = args.num_exp
+    FT_STATE = args.state
+    LOSS_BOUND = args.loss_bound
+    CONSTRAINT = args.constraint
+    TASK = args.task
     
     if ALG_TYPE == 'sg':
         G_ALPHA = args.geomp
@@ -130,12 +125,12 @@ if __name__ == "__main__":
     elif ALG_TYPE == 'sslalm':
         epochs = args.epochs
         BATCH_SIZE = args.batch_size
-        lambda_bound = 100,
-        rho = 1,
-        mu = 2.,
-        tau = 1e-3,
-        beta = 0.1,
-        eta = 5e-3,
+        lambda_bound = 100
+        rho = args.rho
+        mu = args.mu
+        tau = args.tau
+        beta = args.beta
+        eta = 5e-3
         
         
     if ALG_TYPE == 'sg':
@@ -153,22 +148,22 @@ if __name__ == "__main__":
     
     DTYPE = torch.float32
 
-    DATASET_NAME = 'employment_az'
-    FT_DATASET, FT_STATE = DATASET_NAME.split('_')
+    FT_DATASET = TASK
     torch.set_default_dtype(DTYPE)
+    DATASET_NAME = FT_DATASET + '_' + FT_STATE
     
     X_train, y_train, [w_idx_train, nw_idx_train], X_test, y_test, [w_idx_test, nw_idx_test] = load_folktables_torch(
-        FT_DATASET, state=FT_STATE.upper(), random_state=0, make_unbalanced = False
+        FT_DATASET, state=FT_STATE.upper(), random_state=42, make_unbalanced = False, onehot=False
     )
         
     X_train_tensor = tensor(X_train, dtype=DTYPE)
     y_train_tensor = tensor(y_train, dtype=DTYPE)
     train_ds = TensorDataset(X_train_tensor,y_train_tensor)
-    print(f'Train data loaded: {DATASET_NAME}')
+    print(f'Train data loaded: {(FT_DATASET, FT_STATE)}')
+    print(f'Data shape: {X_train_tensor.shape}')
     
     # TODO: move to command line args
     # EXP_NUM = 7
-    LOSS_BOUND = 0.005
     RUNTIME_LIMIT = 15
     UPDATE_LAMBDA = True
     # G_ALPHA = 0.3
@@ -242,18 +237,22 @@ if __name__ == "__main__":
                               update_lambda=UPDATE_LAMBDA,
                               maxiter=MAXITER_ALM,
                               device=device,
+                              epochs=epochs,
                               seed=EXP_IDX)
         elif ALG_TYPE.startswith('sslalm'):
-            history = SSLPD(net, train_ds, w_idx_train, nw_idx_train, loss_bound=LOSS_BOUND,
-                         lambda_bound = 100,
-                         rho = 1,
-                         mu = 2.,
-                         tau = 1e-3,
-                         beta = 0.1,
-                         eta = 5e-3,
-                         max_iter=MAXITER_SSLALM,
-                         device=device,
-                         seed=EXP_IDX)
+            history = SSLPD(net, train_ds, w_idx_train, nw_idx_train,
+                            loss_bound=LOSS_BOUND,
+                            epochs=epochs,
+                            batch_size=BATCH_SIZE,
+                            lambda_bound = 10.,
+                            rho = rho,
+                            mu = mu,
+                            tau = tau,
+                            beta = beta,
+                            eta = 5e-2,
+                            max_iter=MAXITER_SSLALM,
+                            device=device,
+                            seed=EXP_IDX)
         ## SAVE RESULTS ##
         ftrial.append(pd.Series(history['loss']))
         ctrial.append(pd.DataFrame(history['constr']))
@@ -291,11 +290,11 @@ if __name__ == "__main__":
     full_stats = pd.DataFrame(index=index, columns=['Loss', 'C1', 'C2', 'SampleSize', 'time'])
     full_stats.sort_index(inplace=True)
     
-    net = SimpleNet(in_shape=X_test.shape[1], out_shape=1, dtype=DTYPE).cuda()
+    net = SimpleNet(in_shape=X_test.shape[1], out_shape=1, dtype=DTYPE).to(device)
     loss_fn = nn.BCEWithLogitsLoss()
     
-    X_test_tensor = tensor(X_test, dtype=DTYPE).cuda()
-    y_test_tensor = tensor(y_test, dtype=DTYPE).cuda()
+    X_test_tensor = tensor(X_test, dtype=DTYPE).to(device)
+    y_test_tensor = tensor(y_test, dtype=DTYPE).to(device)
     
     X_test_w = X_test_tensor[w_idx_test]
     y_test_w = y_test_tensor[w_idx_test]
@@ -307,28 +306,50 @@ if __name__ == "__main__":
     X_train_nw = X_train_tensor[nw_idx_train]
     y_train_nw = y_train_tensor[nw_idx_train]
     
-    save_train = False
+    save_train = True
+    
+    
     with torch.inference_mode():
         for exp_idx in range(EXP_NUM):
             weights_to_eval = wtrial[exp_idx][::TEST_SKIP_ITERS]
             for alg_iteration, w in enumerate(weights_to_eval):
-
+                
+                if CONSTRAINT == 'loss':
+                    c_f = one_sided_loss_constr
+                    c_loss_fn = nn.BCEWithLogitsLoss()
+                elif CONSTRAINT == 'fpr':
+                    c_f = fairret_constr
+                    statistic = FalsePositiveRate()
+                    c_loss_fn = NormLoss(statistic)
+                elif CONSTRAINT == 'pr':
+                    c_f = fairret_pr_constr
+                    statistic = PositiveRate()
+                    c_loss_fn = NormLoss(statistic)
                 print(f'{exp_idx} | {alg_iteration}', end='\r')
                 net.load_state_dict(w)
                 
                 if save_train:
                     outs = net(X_train_tensor)
-                    loss = loss_fn(outs, y_train_tensor.unsqueeze(1)).detach().cpu().numpy()
+                    if y_train_tensor.ndim < outs.ndim:
+                        y_train_tensor = y_train_tensor.unsqueeze(1)
+                    loss = loss_fn(outs, y_train_tensor).detach().cpu().numpy()
                     
-                    c1 = one_sided_loss_constr(loss_fn, net, [(X_train_w, y_train_w), (X_train_nw, y_train_nw)]).detach().cpu().numpy()
+                    c1 = c_f(c_loss_fn, net, [(X_train_w, y_train_w), (X_train_nw, y_train_nw)]).detach().cpu().numpy()
                     c2 = -c1
                     # pandas multiindex bug(?) workaround
-                    full_stats.loc['train'].at[alg_iteration, exp_idx] = {'Loss': loss, 'C1': c1, 'C2': c2, 'SampleSize': 1}
+                    full_stats.loc['train'].at[alg_iteration, exp_idx] = {
+                    'Loss': loss,
+                    'C1': c1,
+                    'C2': c2,
+                    'SampleSize': samples_trial[exp_idx][alg_iteration],
+                    'time': ttrial[exp_idx][alg_iteration]}
                     
                 outs = net(X_test_tensor)
-                loss = loss_fn(outs, y_test_tensor.unsqueeze(1)).detach().cpu().numpy()
+                if y_test_tensor.ndim < outs.ndim:
+                    y_test_tensor = y_test_tensor.unsqueeze(1)
+                loss = loss_fn(outs, y_test_tensor).detach().cpu().numpy()
                 
-                c1 = one_sided_loss_constr(loss_fn, net, [(X_test_w, y_test_w), (X_test_nw, y_test_nw)]).detach().cpu().numpy()
+                c1 = c_f(c_loss_fn, net, [(X_test_w, y_test_w), (X_test_nw, y_test_nw)]).detach().cpu().numpy()
                 c2 = -c1
                 
                 full_stats.loc['test'].at[alg_iteration, exp_idx] = {

src/experiments/utils/load_folktables.py
@@ -7,13 +7,14 @@ from sklearn.preprocessing import StandardScaler
 
 sys.path.append("..")
 
-from folktables import ACSDataSource, ACSPublicCoverage, ACSEmployment
+from folktables import ACSDataSource, ACSPublicCoverage, ACSEmployment, ACSIncome, generate_categories
 
 RAC1P_WHITE = 1
 
-def load_folktables_torch(dataset: str = 'employment', state='AL', random_state=None, make_unbalanced=False):
+def load_folktables_torch(dataset: str = 'employment', state='AL', random_state=None, onehot=True, make_unbalanced=False):
     data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'raw_data', dataset))
     data_source = ACSDataSource(survey_year=2018, horizon='1-Year', survey='person', root_dir=data_dir)
+    definition_df = data_source.get_definitions(download=True)
     acs_data = data_source.get_data(states=[state], download=True)
     # group here refers to race (RAC1P)
     if dataset == 'employment':
@@ -22,7 +23,19 @@ def load_folktables_torch(dataset: str = 'employment', state='AL', random_state=
         features = features[:,:-1]
     elif dataset == 'coverage':
         features, label, group = ACSPublicCoverage.df_to_numpy(acs_data)
+    elif dataset == 'income':
+        if onehot:
+            categories = generate_categories(features=ACSIncome.features, definition_df=definition_df)
+            features, label, group = ACSIncome.df_to_pandas(acs_data, categories=categories, dummies=True)
+            sens_features = [col for col in features.columns if col.startswith('RAC1P')]
+            features = features.drop(columns=sens_features).to_numpy(dtype='float')
+            label = label.to_numpy(dtype='float')
+        else:
+            features, label, group = ACSIncome.df_to_numpy(acs_data)
+            # drop the RAC1P column
+            features = features[:,:-1]
     
+    # drop sensitive
     group_binary = (group == RAC1P_WHITE).astype(float)
         
     # stratify by binary race (white vs rest)
@@ -48,6 +61,8 @@ def load_folktables_torch(dataset: str = 'employment', state='AL', random_state=
     
     test_w_idx = np.argwhere(g_test == 1).flatten()
     test_nw_idx = np.argwhere(g_test != 1).flatten()
+    
+    # pdX_train_scaled
     
     return X_train_scaled, y_train, [train_w_idx, train_nw_idx], X_test_scaled, y_test, [test_w_idx, test_nw_idx]
     